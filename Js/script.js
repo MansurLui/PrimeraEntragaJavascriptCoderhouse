9ec91303b0ae50afb26a92914ee36fe45f4fc1c8
Agustin Mansur <agustinmansur@gmail.com>
2022-10-01 15:54:44 -0300

    primer commit

---
+++ Js/script.js
@@ -0,0 +1,38 @@
+alert("** BIENVENIDO A MI CALCULADORA DE PRESTAMOS **")
+let nombre = prompt("Ingrese su nombre")
+let dineroIngresado = parseInt(prompt("Inrgesa el monto de dinero que necesitas"))
+let cuotas = parseInt(prompt("ingresá la cantidad de cuotas para devolver el préstamo (puede ser en 6, 12, 18 o 24 cuotas)"))
+
+
+const seiscuotas = dineroIngresado*1.3
+const docecuotas = dineroIngresado*1.75
+const dieciochocuotas = dineroIngresado*2
+const veinticuartrocuotas = dineroIngresado*2.6
+const en6 = seiscuotas/6
+const en12 = docecuotas/12
+const en18 = dieciochocuotas/18
+const en24 = veinticuartrocuotas/24
+
+
+function calculadoraPrestamo() {
+    if (cuotas === 6) {
+        alert("Pagarias 6 cuotas de $" + en6 + " sumando un total: $" + seiscuotas )
+    }
+    else if (cuotas === 12){
+        alert("Pagarias 12 cuotas de $" + en12 + " sumando un total: $" + docecuotas)
+    }
+    else if (cuotas === 18){
+        alert("Pagarias 18 cuotas de $" + en18 + " sumando un total: $" + dieciochocuotas)
+    }
+    else if (cuotas === 24){
+        alert("Pagarias 24 cuotas de $" + en24 + " sumando un total: $" + veinticuartrocuotas)
+    }
+    else if (cuotas !=6 && cuotas !=12 && cuotas != 18 && cuotas !=24){
+        alert("Las cuotas disponibles que tenemos disponibles son en 6, 12, 18 o 24")
+        alert("Recarga la pagina para volver a empezar.")
+    }
+}
+
+calculadoraPrestamo()
+
+alert("Gracias " + nombre +" por utlizar mi calculadora de prestamos")
